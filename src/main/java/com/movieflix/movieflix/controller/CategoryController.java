f3a3a8e5d448991dac8c72e77708355ad5c1c935
cainabb <caina_bratz@hotmail.com>
2026-01-30 16:34:58 -0300

    ResposeEntity colocado em Getall, getbyid, delete e post.

--- src/main/java/com/movieflix/movieflix/controller/CategoryController.java
+++ src/main/java/com/movieflix/movieflix/controller/CategoryController.java
@@ -7,6 +7,7 @@ import com.movieflix.movieflix.response.CategoryResponse;
 import com.movieflix.movieflix.service.CategoryService;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,32 +31,26 @@ public class CategoryController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<> getCategoriesById(@PathVariable Long id){
-        Optional<Category> optionalCategory=categoryService.findById(id);
-        if(optionalCategory.isPresent()){
-            return CategoryMapper.toCategoryResponse(optionalCategory.get());
-        } else
-            return null;
+    public ResponseEntity<CategoryResponse> getCategoriesById(@PathVariable Long id) {
+
+        return categoryService.findById(id)
+                .map(category ->
+                        ResponseEntity.ok(
+                                CategoryMapper.toCategoryResponse(category))).
+                orElse(ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{id}")
-    public void deleteCategoriesByID(@PathVariable Long id){
+    public ResponseEntity<Void> deleteCategoriesByID(@PathVariable Long id){
         categoryService.deleteByID(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 
     @PostMapping
-    public CategoryResponse saveCategory(@RequestBody CategoryRequest request){
+    public ResponseEntity<CategoryResponse> saveCategory(@RequestBody CategoryRequest request){
         Category newCategory = CategoryMapper.toCategory(request);
         Category savedCategory = categoryService.saveCategory(newCategory);
-    return CategoryMapper.toCategoryResponse(savedCategory);
-    }
-
-
-
-
-
-
-
-
+    return ResponseEntity.status(HttpStatus.CREATED).body(CategoryMapper.toCategoryResponse(savedCategory));
+      }
 }
